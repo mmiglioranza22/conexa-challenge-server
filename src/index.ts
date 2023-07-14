78c80639f5b9ee60d5e4c0d15d6806cd270d6edc
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-07-14 12:40:32 -0300

    Procfile

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://conexa-challenge-omega.vercel.app/'
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(morgan('tiny'))
 app.use('/', router)
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at PORT:${port}`);
 });
